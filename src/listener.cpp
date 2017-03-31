a69138076a0b7ff32fcbdc04c9225b12a83e6e3f
htsai51 <htsai51@terpmail.umd.edu>
2017-03-30 23:39:45 -0400

    Add service & client and logging features in Talker/Listner nodes
    1. Add service in Talker to update name in message published on
       chatter topic
    2. Add client in Listener to call Talker service to update name
       in message published on chatter topic
    3. Use ROS logging features (DEBUG, INFO, WARN, ERROR, FATAL) in
       Talker and Listener nodes
    4. Support argument in Talker to set the publish frequecy of Talker
       node on chatter topic
    5. Add doxeygen comments

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,84 +1,95 @@
-/*
- * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include <ros/ros.h>
-#include <std_msgs/String.h>
+/********************************************************************
+ *   MIT License
+ *  
+ *   Copyright (c) 2017 Huei-Tzu Tsai
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ ********************************************************************/
 
 /**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-void chatterCallback(const std_msgs::String::ConstPtr& msg) {
-  ROS_INFO("[Jane] I heard: [%s]", msg->data.c_str());
-}
+ *  @file listener.cpp
+ *  @brief File of main function for listener node
+ *
+ *  This file contains the main program of listener node which subscirbes
+ *  to chatter topic.
+ *
+ *  This program demonstrate the concept of subscribing topic in ROS.
+ *
+ *
+ *  @author Huei Tzu Tsai
+ *  @date   03/30/2017
+*/
+
+#include "listener_class.hpp"
+#include "beginner_tutorials/talkerService.h"
+
 
 int main(int argc, char **argv) {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-  ros::init(argc, argv, "listener");
+    bool ret;
+    Listener listener;
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  ros::NodeHandle n;
+    ros::init(argc, argv, "listener");
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+    ros::NodeHandle n;
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-  ros::spin();
+    // Register client with the master
+    ros::ServiceClient client =
+        n.serviceClient<beginner_tutorials::talkerService>("talkerService");
 
-  return 0;
+    // Wait for service becomes ready (timeout 60 seconds)
+    ret = ros::service::waitForService("talkerService", 60000);
+
+    if (ret == false) {
+        ROS_WARN_STREAM("Timout waiting for talkerService");
+    } else {
+        // Create request & response objects
+        beginner_tutorials::talkerService::Request req;
+        beginner_tutorials::talkerService::Response resp;
+
+        // Fill in request data
+        req.name = "Jane";
+
+        // Call service
+        ret = client.call(req, resp);
+
+        // Check if service call is successful
+        if (ret == true) {
+            ROS_INFO_STREAM("Set talker name " << resp.resp);
+        } else {
+            ROS_ERROR_STREAM("Failed to set talker name");
+        }
+    }
+
+    // Subscribe topic chatter from master to receive messages published
+    // by this topic
+    ros::Subscriber sub = n.subscribe("chatter", 1000,
+                                      &Listener::chatterCallback, &listener);
+
+    if (sub) {
+        ROS_DEBUG_STREAM("Subscribe OK");
+    } else {
+        ROS_FATAL_STREAM("Subscribe FAILED.  Exit listener.");
+        return -1;
+    }
+
+    ros::spin();
+
+    return 0;
 }
 
